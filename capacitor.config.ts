0f8f892f536fe580f74ba2449748011b6e830ff7
Crakchi <crakchi@gmail.com>
2024-04-26 10:19:23 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'SumativaAP5_Rivera_Notificaciones',
+  webDir: 'www'
+};
+
+export default config;
